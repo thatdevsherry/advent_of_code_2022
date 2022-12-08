02603522081892307a6db649c36e3f82a66a2e9e
Shehriyar Qureshi <thatdevsherry@gmail.com>
2022-12-08 10:06:00 +0500

    feat(day-7): WIP first part

--- src/day_7/first_part.rs
+++ src/day_7/first_part.rs
@@ -1,4 +1,10 @@
-use std::{collections::HashMap, io::BufRead, str::FromStr};
+use std::{
+    collections::HashMap,
+    fs::File as FsFile,
+    io::{BufRead, BufReader, Lines},
+    iter::Peekable,
+    str::FromStr,
+};
 
 use crate::read_file;
 
@@ -67,6 +73,15 @@ enum Command {
 }
 
 impl Command {
+    fn get_directory_name(command: Command) -> Result<String, String> {
+        match command {
+            Command::ChangeDirectory(directory) => Ok(directory),
+            _ => Err("Invalid command".to_string()),
+        }
+    }
+}
+
+impl Command {
     fn remove_shell_sign(s: &str) -> &str {
         match s.starts_with('$') {
             true => s.get(2..).unwrap(),
@@ -118,81 +133,86 @@ impl FromStr for Command {
     }
 }
 
+fn handle_ls_output(line_data: String) -> (String, DirectoryContent) {
+    let ls_command_output_type = ListCommandOutput::from_str(&line_data).unwrap();
+    match ls_command_output_type {
+        ListCommandOutput::Directory(name) => {
+            let directory = Directory::new(name.clone());
+            (name, DirectoryContent::Directory(directory))
+        }
+        ListCommandOutput::File(name, size) => {
+            let file = File::new(name.clone(), size);
+            (name, DirectoryContent::File(file))
+        }
+    }
+}
+
+/// This will take in an iter, and we will assume the first item will be the
+/// command "cd {something}".
+///
+/// It will then assume the next command is `ls`, and it will store everything
+/// in a `Directory`. If there is a nested directory, it will recursively
+/// call itself, and pass along the iter recursively.
+///
+/// The recursive iteration will then handle the nested directory.
+fn recursive_file_size_reader(line_iter: &mut Peekable<Lines<BufReader<FsFile>>>) -> Directory {
+    let first_command = Command::from_str(&line_iter.next().unwrap().unwrap()).unwrap();
+    println!("First command (cd): {first_command:?}");
+    let mut directory = Directory::new(Command::get_directory_name(first_command).unwrap());
+
+    let second_command = Command::from_str(&line_iter.next().unwrap().unwrap()).unwrap();
+    println!("Second command (ls): {second_command:?}");
+
+    while Command::from_str(line_iter.peek().unwrap().as_ref().unwrap()).is_err() {
+        let ls_output = line_iter.next().unwrap().unwrap();
+        let (name, content) = handle_ls_output(ls_output);
+        directory.add_content(name, content);
+    }
+
+    // The last command is `ls`, so we also need to check if there even is a next command.
+    // If not, then we are at the last recursion and have to return early, after `ls` output.
+    let next_command_peek = line_iter.peek().unwrap().as_ref();
+    if next_command_peek.is_err() {
+        // return early
+        return directory;
+    }
+    let next_cd_command = Command::from_str(line_iter.peek().unwrap().as_ref().unwrap()).unwrap();
+    println!("Next command (peek): {next_cd_command:?}");
+
+    match next_cd_command {
+        Command::ListContents => panic!(),
+        Command::ChangeDirectory(direction) => {
+            if direction == ".." {
+                // return from this recursive fn
+                line_iter.next();
+                return directory;
+            } else {
+                // start another recursion
+                let recursive_directory = recursive_file_size_reader(line_iter);
+                // line_iter.next();
+                println!("Returned from {:?}", recursive_directory.name);
+                let peep = line_iter.peek().unwrap().as_ref().unwrap();
+                println!("Next command peek: {:?}", peep);
+                let recursive_directory_name = recursive_directory.name.clone();
+                directory.contents.insert(
+                    recursive_directory_name,
+                    DirectoryContent::Directory(recursive_directory),
+                );
+            }
+        }
+    };
+
+    directory
+}
+
 #[allow(dead_code)]
 fn run_day_seven_part_one(filename: &str) -> u32 {
     let reader = read_file(filename);
-    let mut line_iter = reader.lines();
-    let first_line = line_iter.next().unwrap().unwrap();
+    let mut line_iter = reader.lines().peekable();
 
-    // first command is to cd into root.
-    let first_command = Command::from_str(&first_line).unwrap();
-    debug_assert!(first_command == Command::ChangeDirectory("/".to_string()));
-
-    let mut starting_directory;
-    if let Command::ChangeDirectory(ref directory_name) = first_command {
-        starting_directory = Directory::new(directory_name.to_owned());
-    } else {
-        panic!("Umm first command isn't root?")
-    }
-
-    println!("Starting dir: {starting_directory:?}");
-
-    let mut command_last_read = first_command;
-    let mut traversal_stack: Vec<&mut Directory> = vec![&mut starting_directory];
-    let mut nested_indicator = "-".to_string();
-
-    for line in line_iter {
-        let line_data = line.unwrap();
-        // check if last command read was `ls`. If so, do not read the next commands
-        // as actual commands, but parse them as `ls` output and check if any of them
-        // start with "$", which would mean the `ls` output is complete and we have
-        // received a new command.
-        if !line_data.starts_with('$') && command_last_read == Command::ListContents {
-            let ls_command_output_type = ListCommandOutput::from_str(&line_data).unwrap();
-            match ls_command_output_type {
-                ListCommandOutput::Directory(name) => {
-                    let dir = Directory::new(name.clone());
-                    traversal_stack
-                        .last_mut()
-                        .unwrap()
-                        .add_content(name, DirectoryContent::Directory(dir));
-                }
-                ListCommandOutput::File(name, size) => {
-                    let file = File::new(name.clone(), size);
-                    traversal_stack
-                        .last_mut()
-                        .unwrap()
-                        .add_content(name, DirectoryContent::File(file));
-                }
-            };
-            continue;
-        }
-
-        let command = Command::from_str(&line_data).unwrap();
-        println!("{command:?}");
-        if let Command::ChangeDirectory(ref direction) = command {
-            if direction == ".." {
-                nested_indicator.pop();
-                traversal_stack.pop();
-            } else {
-                nested_indicator.push('-');
-                let dir = traversal_stack
-                    .last_mut()
-                    .unwrap()
-                    .contents
-                    .get_mut(direction)
-                    .unwrap();
-                match dir {
-                    DirectoryContent::Directory(dir) => traversal_stack.push(dir),
-                    _ => panic!(),
-                };
-            }
-        }
-        command_last_read = command;
-    }
-    println!("The whole thing");
-    println!("{starting_directory:?}");
-
+    let result = recursive_file_size_reader(&mut line_iter);
+    println!("Result");
+    println!("{:?}", result);
     0
 }
 
